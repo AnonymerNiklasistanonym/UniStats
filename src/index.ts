8bc5947a11036cd2b399bb2de5b924e0326c921b
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2019-03-29 23:49:27 +0100

    Proof of concept read JSON data with type in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-console.log("hi hey")
-console.log("WOW")
+import * as fs from "fs"
+import * as path from "path"
+import { UniTemplate } from "../templates/uni_template"
+
+const demoDataFilePath = path.join(__dirname, "../data/demo.json")
+const demoData: UniTemplate = JSON.parse(fs.readFileSync(demoDataFilePath, "utf8"))
+console.log(demoData)
+console.log(demoData.name.first_name, demoData.name.surname,
+    "studying", demoData.field_of_study,
+    "in the", demoData.current_semester + "th", "semester")
