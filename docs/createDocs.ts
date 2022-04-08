f0cc7e2181f7e1606eb61d10037607594405a975
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2022-04-08 20:24:46 +0200

    Update dependencies

--- docs/createDocs.ts
+++ docs/createDocs.ts
@@ -1,5 +1,4 @@
-import { Application, TSConfigReader, TypeDocReader, SourceFileMode } from "typedoc";
-import { ScriptTarget } from "typescript";
+import { Application, TSConfigReader, TypeDocReader } from "typedoc";
 import path from "path";
 
 
@@ -10,26 +9,29 @@ app.options.addReader(new TSConfigReader());
 app.options.addReader(new TypeDocReader());
 
 app.bootstrap({
-    mode: SourceFileMode.File,
-    target: ScriptTarget.ESNext,
-    experimentalDecorators: true,
-    ignoreCompilerErrors: false,
     categorizeByGroup: true,
+    entryPoints: [
+        path.join(__dirname, "..", "src", "index.ts"),
+        path.join(__dirname, "..", "src", "createHtmlParts.ts"),
+        path.join(__dirname, "..", "src", "getModuleInfos.ts"),
+        path.join(__dirname, "..", "src", "util.ts"),
+    ],
     exclude: [
-        "node_modules/**/*",
-        "docs/**/*",
-        "dist/**/*"
+        path.join(__dirname, "..", "node_modules/**/*"),
+        path.join(__dirname, "..", "docs/**/*"),
+        path.join(__dirname, "..", "dist/**/*")
     ],
     name: "UniStats",
     readme: path.join(__dirname, "DOCS_README_TYPEDOC.md")
 });
 
-const project = app.convert(app.expandInputFiles(["src"]));
+const project = app.convert();
 
 if (project) {
     // Rendered docs
     const outputDir = path.join(__dirname, "dist");
-    app.generateDocs(project, outputDir);
+    // eslint-disable-next-line no-console
+    app.generateDocs(project, outputDir).then(() => console.log("successful")).catch(err => console.error(err));
 } else {
     throw Error("TypeDoc documentation was not successful");
 }
